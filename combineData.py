378eb4d7fcc3eeb5e329742aad4a967169c22bd6
Duc Nguyen <buuduc1999@gmail.com>
2021-06-25 23:42:25 +0700

    combine done

--- combineData.py
+++ combineData.py
@@ -2,26 +2,33 @@ import pandas as pd
 import csv
 df = pd.read_csv('devices.csv')
 # df.iloc[2][1]
-k=df.loc[df['maso']=='PD']
-print(df)
+# k=df.loc[df['maso']=='PD']
+# print(df)
+# for index,row in df.iterrows():
+#     print(row['maso'])
 
-with open('output.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+# with open('output.csv') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+with open('outputCombine.csv', 'w') as output:
     line_count = 0
-    with open('outputCombine.csv', 'w') as output:
-        writer = csv.writer(output)
-        for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-                writer.writerow(['maso','name','idpic'])
+    readerPic = pd.read_csv('output.csv')
+    writer = csv.writer(output)
+    for index,row in df.iterrows():
+        if line_count == 0:
+            # print(f'Column names are {", ".join(row)}')
+            line_count += 1
+            writer.writerow(['maso','name','idpic'])
+        else:
+            
+            # print(f'\t{row[0]} works in the {row[1]} department')
+            # id=splitString(row[1])
+            # print(id)
+            # writer.writerow([row[0],id])
+            dataRoot=readerPic.loc[df['maso']==row['maso']]
+            if len(dataRoot)==0:
+                idPic=''
             else:
-                
-                # print(f'\t{row[0]} works in the {row[1]} department')
-                # id=splitString(row[1])
-                # print(id)
-                # writer.writerow([row[0],id])
-                dataRoot=df.loc[df['maso']==row[0]]
-                writer.writerow([row[0],dataRoot.iloc[0][1],row[1]])
-                line_count += 1
-        print(f'Processed {line_count} lines.')
+                idPic=dataRoot.iloc[0][1]
+            writer.writerow([row['maso'],row[1],idPic])
+            line_count += 1
+    print(f'Processed {line_count} lines.')
